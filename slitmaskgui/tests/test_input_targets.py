64bea285474270b63c31ebf457d4f95b23a488c6
Austin Bowman <astrangbow2@gmail.com>
2025-09-08 13:24:31 -0700

    updated integration

--- slitmaskgui/tests/test_input_targets.py
+++ slitmaskgui/tests/test_input_targets.py
@@ -4,11 +4,13 @@ import json
 
 @pytest.fixture
 def sample_target_list():
+    
     with open('slitmaskgui/tests/testfiles/gaia_target_list.json','r') as file:
         return json.load(file)
 
 def test_parsing(sample_target_list):
     target_list = TargetList("slitmaskgui/tests/testfiles/gaia_starlist.txt")
+    
     object = target_list.send_json()
     for star_item in json.loads(object):
         assert star_item in sample_target_list
